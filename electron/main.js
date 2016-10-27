e8e8a7c7d0f07a342fdd84846b0af7f016b67cb9
NerveClasp <nerveclasp@gmail.com>
2016-10-27 23:09:01 +0300

    Nothing much new here. Had a couple of hours to play around. Usual html js selectors do not work here. Will figure out something later.

--- electron/main.js
+++ electron/main.js
@@ -1,9 +1,10 @@
 const {app, BrowserWindow} = require('electron')
+const Timer = require('onion-timer')
 
 let win
 function createWindow() {
   win = new BrowserWindow({width: 800, height: 600})
-  win.loadURL('file://${__dirname}/index.html')
+  win.loadURL(`file://${__dirname}/index.html`)
   win.on('closed', ()=> {
     win = null;
   })
@@ -19,5 +20,26 @@ app.on('window-all-closed', ()=>{
 app.on('activate', ()=>{
   if (win === null){
     createWindow()
+  }
+})
+let interv = 60;
+// let defaultConfig = {
+//     seconds: 0,
+//     minutes: 0,
+//     hours: 1,
+//     pattern: '{hh}:{mm}:{ss}', //default .toString() pattern
+//     onInit: null,  //called at object initiation
+//     onStart: null, //called at .start()
+//     onTick: {
+//       document.getElementById('counter').innerHTML = timer.toString();
+//     },  //called every Second
+//     onStop: null,  //called at .stop() or when timer hits 00:00:00
+//     onPause: null, //called at .pause()
+//     onResume: null //called at .resume()
+//   }
+let timeLeft = "Smoke now";
+let timer = new Timer(interv, {
+  onTick: () =>{
+    timeLeft = timer.toString();
   }
 })
